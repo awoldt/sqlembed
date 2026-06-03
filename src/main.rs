0a9d2a6fb1adfaf9ce289560d2f48fd6df936a9e
awoldt <abreckwoldt23@gmail.com>
2026-06-02 22:20:33 -0400

    can read files from directories

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 mod utils;
 
-fn main() {
-    let file_string = utils::read_pdf("./17029.pdf");
-    if !file_string.is_ok() {
-        println!("{:?}", file_string.err());
-        return;
-    } else {
-        let text = file_string.unwrap();
-        println!("results");
-        println!("{}", text);
+use std::{
+    error::Error,
+    ffi::{OsStr, OsString},
+};
+
+use utils::FileDetail;
+
+use crate::utils::{get_files, is_valid_file_extension, parse_txt};
+fn main() -> Result<(), Box<dyn Error>> {
+    let cwd = std::env::current_dir()?;
+    let mut files: Vec<FileDetail> = vec![]; // this will have all files
+    match get_files(cwd.as_path(), &mut files) {
+        Ok(x) => x,
+        Err(x) => return Err(x),
+    };
+
+    // only use valid files
+    let mut valid_files: Vec<FileDetail> = vec![];
+    for f in files {
+        if !is_valid_file_extension(&f) {
+            continue;
+        }
+        valid_files.push(f);
     }
+
+    println!("files after the function runs {:#?}", valid_files);
+
+    for f in valid_files {
+        if f.extension == "txt".to_string() {
+            let text = match parse_txt(&f) {
+                Ok(x) => x,
+                Err(e) => return Err(format!("{:#?}\nerror while parsing text file", e).into()),
+            };
+            println!("{:?}", text)
+        }
+    }
+
+    return Ok(());
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,93 @@
-use std::{error::Error};
 use pdfsink_rs::PdfDocument;
+use std::{
+    error::Error,
+    ffi::OsString,
+    fs::{self, DirEntry},
+    path::{Path, PathBuf},
+};
 
-struct Chunk {
-    raw_text: String
+const VALID_FILE_EXTENSIONS: [&str; 2] = ["pdf", "txt"];
+
+#[derive(Debug)]
+pub struct FileDetail {
+    pub path: String,
+    pub extension: String,
 }
-struct ChunkResult {
-    chunks: Vec<Chunk>
+
+pub fn get_files(dir: &Path, files: &mut Vec<FileDetail>) -> Result<(), Box<dyn Error>> {
+    // this function will read all files in a directory
+    // including files within all nested directories
+
+    // it will only return files that are "accepted" file extensions
+
+    if dir.is_dir() {
+        for e in fs::read_dir(dir)? {
+            let entry: DirEntry = e?;
+            let path: PathBuf = entry.path();
+
+            // recursive loop
+            // read nested dir
+            if path.is_dir() {
+                get_files(&path.as_path(), files)?;
+                continue;
+            }
+            if path.extension().is_none() {
+                continue;
+            }
+
+            let ext = path.extension().unwrap();
+            if ext.to_str().is_none() {
+                continue;
+            }
+            let ext_str = ext.to_str().unwrap();
+            let path_str = path.to_string_lossy().into_owned();
+
+            files.push(FileDetail {
+                path: path_str,
+                extension: ext_str.to_string(),
+            });
+        }
+
+        return Ok(());
+    }
+
+    // single file
+    let path_str: String = dir.to_string_lossy().into_owned();
+    if dir.extension().is_none() {
+        return Ok(()); // early return
+    }
+
+    let ext = dir.extension().unwrap();
+    if ext.to_str().is_none() {
+        return Ok(()); // early return
+    }
+    let ext_str = ext.to_str().unwrap();
+
+    files.push(FileDetail {
+        path: path_str,
+        extension: ext_str.to_string(),
+    });
+
+    Ok(())
 }
 
-pub fn read_pdf(path: &str) -> Result<String, Box<dyn Error>> {
-    let file = PdfDocument::open(path)?;
-    let text = file.extract_text();
-    Ok(text)
+pub fn is_valid_file_extension(file: &FileDetail) -> bool {
+    if VALID_FILE_EXTENSIONS.contains(&file.extension.as_str()) {
+        return true;
+    }
+    return false;
 }
 
-pub fn build_chunk_prompt(arg: &str) -> ChunkResult {
-    // this function will build out a vector of 250 word "chunks"
-    // from a massive string argument
+pub fn parse_txt(file: &FileDetail) -> Result<Vec<String>, Box<dyn Error>> {
+    // get the raw string as a text
+    let file_text = fs::read_to_string(&file.path)?;
 
-    let words = arg.split("\n");
-    let str = String::new();
+    // split text by whitespace
+    // and space to get individual words
+    let mut words: Vec<String> = vec![];
+    for word in file_text.trim().split(" ") {
+        words.push(word.to_string());
+    }
+
+    return Ok(words);
 }
