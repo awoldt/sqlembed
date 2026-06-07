728e1ce4195ab711355ac2b35cf9b0a2e3c48b04
awoldt <abreckwoldt23@gmail.com>
2026-06-07 15:25:42 -0400

    added commands to cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, path::PathBuf};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use fastembed::{
     EmbeddingModel::{self, BGESmallENV15},
     ModelInfo, TextEmbedding,
@@ -10,82 +10,96 @@ use crate::utils::VALID_FILE_EXTENSIONS;
 
 #[derive(Parser, Debug)]
 pub struct Args {
-    pub path: Option<String>,
-
-    #[arg(long)]
-    pub exts: Option<String>, // comma seperated string of all the file extensions to parse
-
-    #[arg(long)]
-    pub model: Option<String>, // underlying embedding model to use (must be supported by fastembed crate)
+    #[command(subcommand)]
+    pub command: Commands,
 }
 
-pub struct CliConfig {
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Chunk {
+        path: Option<String>,
+
+        #[arg(long)]
+        exts: Option<String>,
+
+        #[arg(long)]
+        model: Option<String>,
+    },
+}
+
+impl Commands {
+    pub fn get_cli_chunk_config(
+        path: Option<String>,
+        exts: Option<String>,
+        model: Option<String>,
+    ) -> Result<CliChunkConfig, Box<dyn Error>> {
+        let user_defined_path = path;
+        let user_defined_exts = exts;
+
+        // set the custom path to parse files from
+        // if not set... just parse all files in cwd
+        let cwd: PathBuf = std::env::current_dir()?;
+        let path_to_parse: std::path::PathBuf;
+        if let Some(x) = user_defined_path {
+            path_to_parse = cwd.join(x);
+        } else {
+            path_to_parse = cwd;
+        }
+
+        // allow the user to set the file extensions they want to parse
+        // all exts passed here have to be "valid"
+        let mut exts_to_parse: Vec<String> = vec![];
+        if let Some(x) = user_defined_exts {
+            let exts: Vec<&str> = x.split(",").collect();
+            // make sure each extension is valid
+            for e in &exts {
+                if !VALID_FILE_EXTENSIONS.contains(e) {
+                    return Err(format!("{} is not a supported file extension", e).into());
+                }
+                exts_to_parse.push(e.to_string());
+            }
+        }
+
+        // set the model
+        // must be a valid supported model from the fastembed crate
+        let model_to_use: ModelInfo<EmbeddingModel>;
+        if model.is_none() {
+            // if user doesnt pass a model, default to BGESmallENV15
+            let models = TextEmbedding::list_supported_models()
+                .into_iter()
+                .find(|x| x.model == BGESmallENV15);
+
+            if models.is_none() {
+                return Err(format!("error while setting default embedding model").into());
+            }
+
+            model_to_use = models.unwrap();
+        } else {
+            let models = TextEmbedding::list_supported_models()
+                .into_iter()
+                .find(|x| x.model_code == model.clone().unwrap());
+
+            if models.is_none() {
+                return Err(format!(
+                    "{} is not a supported embedding model",
+                    model.clone().unwrap()
+                )
+                .into());
+            }
+
+            model_to_use = models.unwrap();
+        }
+
+        Ok(CliChunkConfig {
+            path_to_parse,
+            exts_to_parse,
+            model_to_use,
+        })
+    }
+}
+
+pub struct CliChunkConfig {
     pub path_to_parse: PathBuf,
     pub exts_to_parse: Vec<String>,
     pub model_to_use: ModelInfo<EmbeddingModel>,
-}
-
-pub fn get_cli_config(cli_args: &Args) -> Result<CliConfig, Box<dyn Error>> {
-    let user_defined_path = &cli_args.path;
-    let user_defined_exts = &cli_args.exts;
-
-    // set the custom path to parse files from
-    // if not set... just parse all files in cwd
-    let cwd: PathBuf = std::env::current_dir()?;
-    let path_to_parse: std::path::PathBuf;
-    if let Some(x) = user_defined_path {
-        path_to_parse = cwd.join(x);
-    } else {
-        path_to_parse = cwd;
-    }
-
-    // allow the user to set the file extensions they want to parse
-    // all exts passed here have to be "valid"
-    let mut exts_to_parse: Vec<String> = vec![];
-    if let Some(x) = user_defined_exts {
-        let exts: Vec<&str> = x.split(",").collect();
-        // make sure each extension is valid
-        for e in &exts {
-            if !VALID_FILE_EXTENSIONS.contains(e) {
-                return Err(format!("{} is not a supported file extension", e).into());
-            }
-            exts_to_parse.push(e.to_string());
-        }
-    }
-
-    // set the model
-    // must be a valid supported model from the fastembed crate
-    let model_to_use: ModelInfo<EmbeddingModel>;
-    if cli_args.model.is_none() {
-        // if user doesnt pass a model, default to BGESmallENV15
-        let models = TextEmbedding::list_supported_models()
-            .into_iter()
-            .find(|x| x.model == BGESmallENV15);
-
-        if models.is_none() {
-            return Err(format!("error while setting default embedding model").into());
-        }
-
-        model_to_use = models.unwrap();
-    } else {
-        let models = TextEmbedding::list_supported_models()
-            .into_iter()
-            .find(|x| x.model_code == cli_args.model.to_owned().unwrap());
-
-        if models.is_none() {
-            return Err(format!(
-                "{} is not a supported embedding model",
-                cli_args.model.to_owned().unwrap()
-            )
-            .into());
-        }
-
-        model_to_use = models.unwrap();
-    }
-
-    Ok(CliConfig {
-        path_to_parse,
-        exts_to_parse,
-        model_to_use,
-    })
 }
--- src/main.rs
+++ src/main.rs
@@ -15,86 +15,91 @@ use cli::Args;
 use utils::FileDetail;
 
 use crate::{
-    cli::get_cli_config,
+    cli::{Commands},
     sql::{FilesChunkResults, generate_sql, write_sql_to_filesystem},
     utils::{VALID_FILE_EXTENSIONS, chunk_text, embed_chunks, extract_text_from_file, get_files},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Args = Args::parse();
-    let cli_config: cli::CliConfig = get_cli_config(&args)?;
 
-    let files: Vec<FileDetail> = get_files(
-        &cli_config.path_to_parse.as_path(),
-        &cli_config.exts_to_parse,
-    )?;
+    match args.command {
+        Commands::Chunk { path, exts, model } => {
+            let cli_config: cli::CliChunkConfig = Commands::get_cli_chunk_config(path, exts, model)?;
 
-    if files.len() == 0 {
-        println!(
-            "there are no files to embed. valid files extensions: {}",
-            VALID_FILE_EXTENSIONS.join(", ")
-        );
-        return Ok(());
-    }
+            let files: Vec<FileDetail> = get_files(
+                &cli_config.path_to_parse.as_path(),
+                &cli_config.exts_to_parse,
+            )?;
 
-    // create the embedding model once outside of all the chunking logic
-    // so doesnt have to be recreated constantly
-    let mut embedding_model: TextEmbedding =
-        TextEmbedding::try_new(InitOptions::new(EmbeddingModel::BGESmallENV15))?;
+            if files.len() == 0 {
+                println!(
+                    "there are no files to embed. valid files extensions: {}",
+                    VALID_FILE_EXTENSIONS.join(", ")
+                );
+                return Ok(());
+            }
 
-    let mut file_results: Vec<FilesChunkResults> = vec![];
+            // create the embedding model once outside of all the chunking logic
+            // so doesnt have to be recreated constantly
+            let mut embedding_model: TextEmbedding =
+                TextEmbedding::try_new(InitOptions::new(EmbeddingModel::BGESmallENV15))?;
 
-    let pb: ProgressBar = ProgressBar::new_spinner();
-    pb.set_style(ProgressStyle::with_template("{spinner} {msg}")?);
-    pb.enable_steady_tick(Duration::from_millis(100));
+            let mut file_results: Vec<FilesChunkResults> = vec![];
 
-    let start = Instant::now();
+            let pb: ProgressBar = ProgressBar::new_spinner();
+            pb.set_style(ProgressStyle::with_template("{spinner} {msg}")?);
+            pb.enable_steady_tick(Duration::from_millis(100));
 
-    for (i, f) in files.iter().enumerate() {
-        pb.set_message(format!(
-            "File {:?}/{:?} | {:?}",
-            i + 1,
-            files.len(),
-            f.filename
-        ));
+            let start = Instant::now();
 
-        // 1. extract text from files
-        let file_text = extract_text_from_file(&f)?;
+            for (i, f) in files.iter().enumerate() {
+                pb.set_message(format!(
+                    "File {:?}/{:?} | {:?}",
+                    i + 1,
+                    files.len(),
+                    f.filename
+                ));
 
-        // 2. extract chunks from text
-        let mut chunks = chunk_text(&file_text);
+                // 1. extract text from files
+                let file_text = extract_text_from_file(&f)?;
 
-        // 3. embed each chunk and set the embedding field on the struct
-        embed_chunks(&mut chunks, &mut embedding_model)?;
+                // 2. extract chunks from text
+                let mut chunks = chunk_text(&file_text);
 
-        file_results.push(FilesChunkResults {
-            filename: f.filename.clone(),
-            file_extention: f.extension.clone(),
-            chunks,
-        });
-    }
+                // 3. embed each chunk and set the embedding field on the struct
+                embed_chunks(&mut chunks, &mut embedding_model)?;
 
-    pb.finish_and_clear();
+                file_results.push(FilesChunkResults {
+                    filename: f.filename.clone(),
+                    file_extention: f.extension.clone(),
+                    chunks,
+                });
+            }
 
-    let sql_string = generate_sql(&file_results, cli_config.model_to_use)?;
-    write_sql_to_filesystem(&sql_string)?;
+            pb.finish_and_clear();
 
-    let num_of_chunks = {
-        let mut i = 0;
-        for f in file_results {
-            i += f.chunks.len()
-        }
-        i
-    };
+            let sql_string = generate_sql(&file_results, cli_config.model_to_use)?;
+            write_sql_to_filesystem(&sql_string)?;
 
-    println!(
-        "\n\n=======================
+            let num_of_chunks = {
+                let mut i = 0;
+                for f in file_results {
+                    i += f.chunks.len()
+                }
+                i
+            };
+
+            println!(
+                "\n\n=======================
 Successfully parsed {} files and generated {} chunks in {:.2?} seconds
     ",
-        files.len(),
-        num_of_chunks,
-        start.elapsed()
-    );
+                files.len(),
+                num_of_chunks,
+                start.elapsed()
+            );
 
-    return Ok(());
+            return Ok(());
+        }
+    }
 }
